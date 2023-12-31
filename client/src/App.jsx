818207677753fe96f432765ba68fc0a654a477f4
Nabidbhai12 <1905013@ugrad.cse.buet.ac.bd>
2023-12-31 18:06:53 +0600

    search page has been initiated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import Search from './pages/Search';
 
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/search" element={<Search />} />
      <Route element ={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile />} />
       </Route>
