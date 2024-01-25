9584c6e78160a2acb72849a2be1af0ede6625ea0
SaffatZabin-17 <1905060@ugrad.cse.buet.ac.bd>
2024-01-25 16:20:35 +0600

    Fixed the font on the homepage

--- client/src/components/LandingPageHeader.jsx
+++ client/src/components/LandingPageHeader.jsx
@@ -118,7 +118,7 @@ const LandingPageHeader = (props) => {
           <div className="flex flex-row gap-10 h-[42px] md:h-auto sm:hidden items-center justify-start w-[228px]">
             <Button
               className="common-pointer bg-transparent cursor-pointer flex items-center justify-center min-w-[94px]"
-              onClick={() => navigate("/listing")}
+              onClick={() => navigate("/search")}
               leftIcon={
                 <Img
                   className="h-6 mb-px mr-2"
